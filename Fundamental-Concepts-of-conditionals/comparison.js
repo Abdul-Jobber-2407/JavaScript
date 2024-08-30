c550818e0efbf4176e9f44cecc70f0b953df612e
AR Abdul Jobber <arjobbar2019@gmail.com>
2024-08-30 07:32:20 +0600

    added to a xompairison

---
+++ Fundamental-Concepts-of-conditionals/comparison.js
@@ -0,0 +1,32 @@
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+var lastExamResult = 86;
+
+/**
+ * OPERATION
+ * 1. +,-,*,/,% 
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4.null
+ * 5.Number
+ */
+
+/**
+ * 1. bigger: >
+ * 2. less:<
+ * 3. equal:==
+ * 4. greater than or equal :>=
+ * 5. less than or equal : >=
+ * 6. not equal to :!
+ * ----------------------------------
+ * 7. and : &&
+ * 8. or :||
+ */
---
+++ Fundamental-Concepts-of-conditionals/variable.js
@@ -0,0 +1,12 @@
+/*
+* VARIABLE ==> CONTAINER
+ */
+//number 
+var orangePrice = 60;
+
+// string
+var subject = "CSE";
+
+// Boolean
+var passed =true;
+
